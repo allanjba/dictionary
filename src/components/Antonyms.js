10d38584fa5a4962ec50eebeb8024c9ff1fff45c
allanjba <allan.ientz@gmail.com>
2022-03-31 14:18:31 -0600

    Antonyms

---
+++ src/components/Antonyms.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Antonyms = ({ all }) => {
+  return (
+    <>
+      {all.map((Val) => {
+        return Val.meanings.map((Means) => {
+          return Means.definitions.map((Def) => {
+            return Def.antonyms.map((Ant) => {
+              return (
+                <>
+                  {Ant}
+                </>
+              );
+            });
+          });
+        });
+      })}
+    </>
+  );
+};
+
+export default Antonyms;
--- src/components/Select.js
+++ src/components/Select.js
@@ -2,28 +2,29 @@ import React from "react";
 import Definition from "./Definition";
 import Example from "./Example";
 import Synonyms from "./Synonyms";
+import Antonyms from "./Antonyms";
 
 const Select = ({ all, main, audio }) => {
   return (
     <div>
-      <div>
+      <div >
         {main.word}
       </div>
-      <div className="">
+      <div >
         meaning & definitions :
       </div>
       <Definition all={all} />
-      <div className="">
+      <div >
         examples :
       </div>
-
-      <Example all={all} />
-
-      <div className="col-12 text-start my-3 text-capitalize fs-4 fw-bold">
+      <div>
         synonyms :
       </div>
       <Synonyms all={all} />
-
+      <div>
+        antonyms :
+      </div>
+      <Antonyms all={all} />
     </div>
   );
 };
